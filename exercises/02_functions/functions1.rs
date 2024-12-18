c1d3590b6e7dc9be06abe17800bd632a7d45fb55
nusakom <2031263745@qq.com>
2024-12-18 21:24:59 +0800

    Update submodule content

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,7 +1,8 @@
 // 定义一个名为 `call_me` 的函数，函数没有参数，也没有返回值
 fn call_me() {
-    for i in 0.num {}
-    println!("Hello from call_me!");
+    for i in 0..5 { // 使用 0..5 作为范围进行循环
+        println!("Hello from call_me! Iteration: {}", i);
+    }
 }
 
 fn main() {
